ba401c2a6846af071ce3bb17563277b954139cf2
Aaron Smith <74211255+amaizingrice@users.noreply.github.com>
2021-01-11 12:40:38 -0600

    Create dogYearCounter.js

---
+++ dogYearCounter.js
@@ -0,0 +1,15 @@
+const myAge = 19;
+
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+const myAgeInDogYears = earlyYears +laterYears;
+
+const name = 'Aaron';
+const myName = name.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years. `)
+
